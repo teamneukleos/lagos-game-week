b9b5f1360369167c65afd51463a512164eaa226f
Oluwatosinakb <oluwatosinakinbobola417@gmail.com>
2026-05-13 14:22:01 +0100

    added changes for about smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
 
       <main className="">
         <Partners2025 />
+        <div id="about-marker" />
         <About />
         <TimelineSection />
         <SecureSpotSection />
